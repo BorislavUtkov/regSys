198e56b33a4bc3a1736462c21b2f00761d3fc606
Borislav <borislavutkov1@gmail.com>
2023-07-17 11:49:31 +0500

    Рефактор signUp, изменил тип данных Users

--- regSys.cpp
+++ regSys.cpp
@@ -2,53 +2,87 @@
 #include "regSys.h"
 
 regSys::regSys(int a_base){
-    printf("base has been created\n");
+    printf("  ______________________\n");
+    printf(" |                      |\n");
+    printf(" |base has been created |\n");
+    printf(" |______________________|\n");
+    
+    //создание гостевого/начального пользователя
+    sampleUser guest;// как конвертировать строку в идентификатор
+    users.push_back(guest);           
+    users.at(0).id=getNextId();
+    strcpy(users.at(0).nickname,"Guest"); //сделать отдельной функцией ?
+    users.at(0).lvl=0;
+    setActiveId(0);
 };
-void regSys::showAll(){
+bool regSys::setActiveId(int ID){
+    if (ID<=counter && ID>=0){
+        activeId=ID;
+        return 0; //успех
+    }
+    else
+        return 1; //ошибка
+}
+int regSys::getNextId() {
+    counter++;
+    return(--counter);
+
+    //? проблема, что требуется считать с 0
+    //? но после return'а счетик не изменить
+    //? решение чтобы не делать костыль в .h
+    //? начиная счетчик с -1
+}
+int regSys::getActiveId()
+{
+    return activeId;
+}
+/*void regSys::showAll(){
     int iteration=0; 
-    for (int i = 0; i < counter; i++)
-    {
+    for (int i = 0; i < counter; i++){
         printf("\n%d) ", i);
         cout << users.at(i) <<" "<< passwords.at(i);
     }
-   
-//    for (auto &&i : users){
-//         cout<< iteration <<") "<< i <<endl;
-//         iteration++;
-//     }
 
-//     iteration=0; 
-//     for (auto &&i : passwords){
-//         cout<< iteration <<") "<< i <<endl;
-//         iteration++;
-//     }
-}
+
+   for (auto &&i : users){
+        cout<< iteration <<") "<< i <<endl;
+        iteration++;
+    }
+
+    iteration=0; 
+    for (auto &&i : passwords){
+        cout<< iteration <<") "<< i <<endl;
+        iteration++;
+    }
+}*/
 
 int regSys::firstMenu()
 { 
+    
     int sM;//? todo func CreateTemp-ScanfTemp-Return temp
-    // cout << "Active user " << activeUser;
-    printf ("\n________________________\n");
-    printf("|1 - Sign in             |\n");
-    printf("|2 - Sign Up             |\n");
-    printf("|3 - Show all users      |\n");
-    printf("|0 - exit                |\n");
-    printf("_________________________\n");
+    printf("\n");
+    printf(" Hello, %s \n", users.at(getActiveId()).nickname);
+    printf(" _________________________\n");
+    printf("| 1 - Sign in             |\n");
+    printf("| 2 - Sign Up             |\n");
+    //printf("| 3 - Show all users      |\n");
+    printf("| 0 - exit                |\n");
+    printf("|_________________________|\n");
     printf("|Enter a number: ");
     scanf("%i",&sM);
     system( "cls" );
-    switch (sM){
+    switch (sM){ //FIXME getchar()-48 (вычет кода ASCII) ? 
     case 1:
-        signIn();
+        //signIn();
         break;
     case 2:
-        if (counter < maxCount || maxCount == -1)
+        if (counter < maxCount || maxCount == -1)           
             signUp();
-        else
+        // else
             printf("We can't to add you in our society \n");
         break;
     case 3:
-        showAll();
+        // showAll();
         break;
     case 0:
         return(0);
@@ -60,101 +94,118 @@ int regSys::firstMenu()
     return(1);
 }
 void regSys::signUp(){
-    errCode =nameErrCode::allRight;
     system( "cls" );
     do{   
-        //todo Проверка доступности символов (через коды таблицы ASCII)
-        switch (errCode)
-        {
-        case nameErrCode::alreadyTaken:
-            cout<<newNickname;
-            printf("\n Nickname already used by other user. ");
-            printf("\nUse some more your creativity, try again. ");
-            break;
-        case nameErrCode::sizeErr:
-            printf("\nSize error, try again. ");
-            break;
-        default:
-            break;
-        }
-        errCode =nameErrCode::allRight;
-        printf("\n0 - Exit: ");
-        printf("\nThe number of characters must be in the range [%d:%d]",NICKMINSYMB, NICKMAXSYMB);
+        // TODO Проверка доступности символов (через коды таблицы ASCII)
+        tmpNickname= "0";
+        printf("\n");
+        printf("  _________________________________\n");
+        printf(" | The number of characters         |\n");
+        printf(" |  should be in the range [%d:%d]   |\n",NICKMINSYMB, NICKMAXSYMB);
+        printf(" |                                  |\n");
+        printf(" |  0 - Exit:                       |\n");
+        printf(" |__________________________________|\n");
         printf("\nEnter new username: ");
-        cin >> newNickname ;
-        if (newNickname=="0"){      
-            errCode=nameErrCode::exitByUser;    //! пользователь решил выйти
-        } else if (newNickname.size()> NICKMAXSYMB || newNickname.size()< NICKMINSYMB){
-            errCode = nameErrCode::sizeErr;     //! ошибка размера имени
-        } else{
-            for (int i = 0; i < counter; i++){  
-                if(newNickname == users[i])     //! имя уже занято  
-                    errCode = nameErrCode::alreadyTaken; 
-            }
-        }
-        system( "cls" );
+        scanf ("%s", tmpNickname) ;
 
-       if(errCode == nameErrCode::allRight){  //проверка флага ошибки
-            printf("\n Hello, ");
-            cout << newNickname;
+        printf("\n - %s", tmpNickname);
+        if (tmpNickname != "0"){//! пользователь решил выйти      
+            if (tmpNickname[NICKMAXSYMB+1] != 0 || tmpNickname[NICKMINSYMB] == 0 ){  //! ошибка размера имени
+                printf("\nSize error, try again. ");
+            } else if (counter > 1){
+                for (int i = 0; i < counter; i++)  
+                    if(tmpNickname == users[i].nickname) {    //! имя уже занято  
+                        printf("\n Nickname already used by other user. ");
+                        printf("\n Please, try again. ");
+                    }
+            } else                                          //проверка флага ошибки
+                printf("Hello, %s", tmpNickname);
         }
-    } while (errCode != nameErrCode::allRight && errCode != nameErrCode::exitByUser);
-   
-    //! создание пароля
-    if(errCode==nameErrCode::allRight){
-        do{
-            newPassword="";
-            printf("\n Now let's create password");
-            printf("\nThe number of characters must be in the range [%d:%d]", PASSMINSYMB, PASSMAXSYMB);
-            printf("\n0 - Exit: ");
-            printf("\nEnter a password: ");
-            cin>>newPassword;
-            if (newPassword.size()<PASSMINSYMB || newPassword.size()>PASSMAXSYMB){
-                system( "cls" );
-                printf("\n Size error ");
-                errCode=nameErrCode::sizeErr;
-            }
-            else
-            {
-                printf("\nConfirm a password: ");
-                cin>>confirmPassword;
-                if (confirmPassword != newPassword)
-                {
-                    errCode = nameErrCode::mismatchConfirm;
-                    system( "cls" );
-                    printf("\nPassword mismatch, try again");
-                }
-                else 
-                    errCode=nameErrCode::allRight;
-            }
-        }while(errCode != nameErrCode::allRight && errCode != nameErrCode::exitByUser);
-    }
+    } while (tmpNickname != "0");
+// //! ************************************************************************************************
+// //! ************************************************************************************************
+// //! ************************************************************************************************
+//     //! Если нет ошибок на этом этапе,
+//     //! то переходим к созданию пароля
+//     if(errCode==nameErrCode::allRight){
+//         do{
+//             tmpPassword="";
 
-    if(errCode == nameErrCode::allRight){   //выход из функции по требованию пользователя
-            counter++;
-            passwords.push_back(newPassword);
-            users.push_back(newNickname);
-            system( "cls" );
-            printf("\n Welcome here "); 
-            cout<<newNickname;
+
+//             printf("  _________________________________\n");
+//             printf(" | Now let's create password        |\n");
+//             printf(" |  should be in the range [%d:%d]   |\n",PASSMINSYMB, PASSMAXSYMB);
+//             printf(" |                                  |\n");
+//             printf(" |  0 - Exit:                       |\n");
+//             printf(" |__________________________________|\n");
+//             printf("\nEnter a password: ");
+//             cin>>tmpPassword;
+//             if (tmpNickname=="0"){      
+//                     errCode=nameErrCode::exitByUser;    //! пользователь решил выйти
+//             } else if (tmpPassword.size()<PASSMINSYMB || tmpPassword.size()>PASSMAXSYMB){
+//                 system( "cls" );
+//                 printf(" Size error \n");
+//                 errCode=nameErrCode::sizeErr;
+//             }
+//             else{
+//                 printf("\nConfirm a password: ");
+//                 cin>>confirmPassword;
+//                 if (confirmPassword != tmpPassword){    //! пароль не совпадает с введенным ранее
+//                     errCode = nameErrCode::mismatchConfirm; 
+//                     system( "cls" );
+//                     printf("\nPassword mismatch, try again\n");
+//                 }else 
+//                     errCode=nameErrCode::allRight;
+//             }
+//         }while(errCode != nameErrCode::allRight && errCode != nameErrCode::exitByUser);
+//     }
+
+
+//     if(errCode == nameErrCode::allRight){   //выход из функции по требованию пользователя
+//             counter++;
+//             // passwords.push_back(tmpPassword);
+//             // users.push_back(tmpNickname);
+//             system( "cls" );
+//             printf("\n Welcome here, ");
+//             cout<<tmpNickname<<endl;
  
-    }  else if(errCode == nameErrCode::exitByUser){
-            newNickname="";
-            printf("\n Return to menu"); 
-    }
+//     }  else if(errCode == nameErrCode::exitByUser){
+//             tmpNickname="";
+//             printf("\n Return to menu"); 
+//     }
  }
 
 
-void regSys::signIn(){
-        printf("\n\n todo\n\n");    
- }
+// int regSys::signIn(){
+//     if (counter>0){
+//         printf("\n Login: "); 
+//         cin >> tmpNickname;
+//         int i=0;
+//         while (i != counter+1) //TODO сделать без +1
+//         {
+            
+//         }
+        
+//             if (users[i] == tmpNickname){
+
+//             }
+        
+//     }
+//     else{
+//         printf("\n Return to menu"); 
+//         return -1;
+//     }
+
+//  }
 //todo UserMenu
 //todo Copy user's information in file.txt 
 //todo SignIn
-//todo password 
+// password 
 void regSys::regSysMain(){
     while (endWork)
     {
+
+        
         endWork =firstMenu();    
     }
 }
--- regSys.h
+++ regSys.h
@@ -2,46 +2,72 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstring>
+
 
 
 using namespace std;
 class regSys
 {
+private:
+    int activeId;             //! id активного пользователя
 protected:
-    enum nameErrCode{
-        allRight=0,
-        exitByUser=1,
-        alreadyTaken=2,
-        sizeErr=3,
-        mismatchConfirm=4
-
-    };
-    static const int PASSMINSYMB = 6;
-    static const int PASSMAXSYMB = 30;
-    static const int NICKMINSYMB = 6;
-    static const int NICKMAXSYMB = 30;
+    // enum nameErrCode{   //Коды ошибок
+    //     allRight=0,
+    //     exitByUser=1,
+    //     alreadyTaken=2,
+    //     sizeErr=3,
+    //     mismatchConfirm=4
+    // };
+    static const int PASSMINSYMB = 6;  //Минимальная длина пароля
+    static const int PASSMAXSYMB = 30; //Максиимальная длина пароля
+    static const int NICKMINSYMB = 6;  //Минимальная длина имени
+    static const int NICKMAXSYMB = 30; //Максиимальная длина имени
     /*
     *@param id Уникальный номер пользователя
-    *@param id Имя соответствующее номеру
-    *@brief Вектор пар <id - username> 
+    *@param nickname Имя соответствующее номеру
+    *@param password Пароль
+    *@param lvl Уровень привилегий
+    *@brief Структура с данными пользователя  
     */
-   
-    // vector<pair<int,char[NICKMAXSYMB+1]>> names; 
-    vector<string> users; 
-    vector<string> passwords; 
+    struct sampleUser{
+        int id;
+        char nickname[NICKMAXSYMB];
+        char password[PASSMAXSYMB];//TODO Своя строка ??? Использовать готовый string
+        int lvl; //Privilege Levels (0 - viewer, 1 - user, 5 - Admin)
+    };
+    vector <sampleUser> users;  //!!!! ГЛАВНЫЙ ВЕКТОР ХРАНЯЩИЙ ВСЕ ЗАПИСИ
+    // vector<string> passwords; 
     int errCode=0;              //!переменная используемая для записывания кода ошибки   
     int counter=0;              //! Счетчик зарегистрированых записей 
     int maxCount=-1;            //! Максимальное количество записей (-1 = infinity)
     bool endWork = 1;           //! Флаг для окончания работы основной функции
-    string newNickname;         //! Временная переменная для создания ника нового пользователя
-    string newPassword;         //! Временная переменная для создания пароля нового пользователя
+    char* tmpNickname;         //! Временная переменная для создания ника нового пользователя
+    string tmpPassword;         //! Временная переменная для создания пароля нового пользователя
     string confirmPassword;     //! Временная переменная для подтверждения пароля нового пользователя
-    string activeUser="";
 public:
+    /*
+    *@brief Получение ID текущего пользователя
+    *@return ID Активного пользователя 
+    */
+    int getActiveId();
+    /*
+    *@brief Назначение текущего пользователя по id
+    *@param ID требуемого пользователя 
+    *@return Код ошибки: 0-успех, 1-ошибка
+    */
+    bool setActiveId(int ID);
+    /*
+    *@brief получение id
+    *@return порядковый id для пользователя
+    */
+    int getNextId();
+   /*
+
     /*
     *@brief вывод всехимеющихся пользователей
     */
-    void showAll();
+    // void showAll();
    /*
     * @brief Регистрация в системе
     */
@@ -49,7 +75,7 @@ public:
     /*
     * @brief Вход в систему
     */
-    void signIn();
+    // int signIn();
     /*
     *@brief Основная функция 
     */
